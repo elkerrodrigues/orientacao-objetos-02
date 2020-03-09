98c48c6dd5a9ab386850beb691a47ce7677640f2
Elker Rodrigues <elker.rodrigues@gmail.com>
2020-03-09 15:06:04 -0300

    Adicionado exceções para a classe Circulo e teste das exceções na classe CirculoTeste

--- src/main/java/exercicios/Circulo.java
+++ src/main/java/exercicios/Circulo.java
@@ -1,14 +1,18 @@
 package exercicios;
 
 public class Circulo {
-    public Double raio;
+    private Double raio;
 
     public Double getRaio() {
         return raio;
     }
 
     public void setRaio(Double raio) {
-        this.raio = raio;
+        if (raio<=0) {
+            throw new IllegalArgumentException("O raio deve ser maior que zero.");
+        }
+        else this.raio = raio;
+
     }
 
     public Double calcularArea(){
--- src/test/java/exercicios/CirculoTeste.java
+++ src/test/java/exercicios/CirculoTeste.java
@@ -2,8 +2,7 @@ package exercicios;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CirculoTeste {
 
@@ -16,29 +15,37 @@ public class CirculoTeste {
     @Test
     public void presencaAtributoBase() {
         Circulo c1 = new Circulo();
-        c1.raio = 10.0;
-        assertNotNull(c1.raio);
+        c1.setRaio(10.0);
+        assertNotNull(c1.getRaio());
     }
 
     @Test
     public void metodoGetRaio() {
         Circulo c1 = new Circulo();
-        c1.raio = 10.0;
+        c1.setRaio(10.0);
         assertEquals(10.0, c1.getRaio());
     }
 
     @Test
     public void metodoSetRaio() {
         Circulo c1 = new Circulo();
-        c1.raio = 10.0;
+        c1.setRaio(10.0);
         c1.setRaio(20.0);
-        assertEquals(20.0, c1.raio);
+        assertEquals(20.0, c1.getRaio());
+    }
+
+    @Test
+    public void metodoSetRaioInvalido() {
+        Circulo c1 = new Circulo();
+        c1.setRaio(10.0);
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> c1.setRaio(-20.0));
+        assertEquals("O raio deve ser maior que zero.", exception.getMessage());
     }
 
     @Test
     public void metodoCalcularArea() {
         Circulo c1 = new Circulo();
-        c1.raio = 10.0;
+        c1.setRaio(10.0);
         Double valorEsperado = 314.0;
         Double valorObtido = c1.calcularArea();
         assertEquals(valorEsperado, valorObtido);
@@ -47,7 +54,7 @@ public class CirculoTeste {
     @Test
     public void metodoCalcularPerimetro() {
         Circulo c1 = new Circulo();
-        c1.raio = 10.0;
+        c1.setRaio(10.0);
         Double valorEsperado = 20.0 * 3.14;
         Double valorObtido = c1.calcularPerimetro();
         assertEquals(valorEsperado, valorObtido);
